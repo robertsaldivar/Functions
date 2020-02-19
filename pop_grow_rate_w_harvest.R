6683a11e9894c3133826d4a57327467918b91d2d
robertsaldivar <rnsaldivar@bren.ucsb.edu>
2020-02-19 09:56:11 -0800

    added error checking to the function

--- pop_grow_rate_w_harvest.R
+++ pop_grow_rate_w_harvest.R
@@ -1,5 +1,11 @@
 pop_grow_rate_w_harvest = function(population, hunters, growth_rate=0.05, carrying_capacity = 2000, attack_rate = 0.05, handling_time = 0.2) {
   
+  #make sure that population is not negative
+  if (population < 0) return("population cannot be less than 0")
+  
+  #make sure that # of hunter is not negative
+  if (hunters < 0) hunters=NA
+  
   results = (growth_rate * population * (1 - population/carrying_capacity) - (hunters * (attack_rate * population)/(1 + attack_rate * handling_time * population)))
   
   return(results)
